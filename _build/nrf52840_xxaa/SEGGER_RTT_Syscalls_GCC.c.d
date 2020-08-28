a855c9f051a29f5e64b44eabeb76ba709c527243
André Quinta <andre.quinta@gmail.com>
2020-08-28 21:02:03 +0100

    Project added

--- _build/nrf52840_xxaa/SEGGER_RTT_Syscalls_GCC.c.d
+++
@@ -1,5 +0,0 @@
-_build/nrf52840_xxaa/SEGGER_RTT_Syscalls_GCC.c.o: \
- /home/altran/nRF5_SDK_16.0.0_98a08e2/external/segger_rtt/SEGGER_RTT_Syscalls_GCC.c \
- /home/altran/workspace/itcx_sdk16/config/sdk_config.h
-
-/home/altran/workspace/itcx_sdk16/config/sdk_config.h:
